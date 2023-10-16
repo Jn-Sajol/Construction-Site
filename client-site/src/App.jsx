de7f711820fecd45a645ccf2bccc7a2ae0bc8b7f
Jn-Sajol <julkar10121@gmail.com>
2023-10-16 19:51:40 +0600

    adding route in app route

--- client-site/src/App.jsx
+++ client-site/src/App.jsx
@@ -1,10 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<Home></Home>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    <Route path="/signin" element={<SignIn/>}/>
+    <Route path="/signout" element={<SignOut/>}/>
     </Routes>
     </BrowserRouter>
   )
