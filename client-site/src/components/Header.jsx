9b06cbddd6e30c2f3cf46e44452bdf64658fe01b
Jn-Sajol <julkar10121@gmail.com>
2023-10-16 22:47:47 +0600

    addingstyile more

--- client-site/src/components/Header.jsx
+++ client-site/src/components/Header.jsx
@@ -1,15 +1,22 @@
 const Header = () => {
   return (
     <>
-      <header className="bg-slate-400 flex justify-between items-center">
-      <div className="flex items-center">
-        <span className="text-3xl bg-blend-color-dodge">Zulkar</span>
-        <span className="text-2xl border-l ">Construction</span>
+      <header className="bg-slate-400 flex justify-between items-center p-3">
+        <div className="flex items-center">
+          <span className="text-3xl bg-blend-color-dodge">Zulkar</span>
+          <span className="text-2xl border-l ">Construction</span>
         </div>
         <div>
-        <form action="">
-        <input type="text" />
-        </form>
+          <form action="">
+            <input type="text" />
+          </form>
+        </div>
+        <div className="">
+          <ul className="flex gap-5">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </div>
       </header>
     </>
