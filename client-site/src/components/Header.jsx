4530bc3b1f7c40a322b9fea47e9d157060b1165c
Jn-Sajol <julkar10121@gmail.com>
2023-10-16 22:43:48 +0600

    adding navbar styile

---
+++ client-site/src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <>
+      <header className="bg-slate-400 flex justify-between items-center">
+      <div className="flex items-center">
+        <span className="text-3xl bg-blend-color-dodge">Zulkar</span>
+        <span className="text-2xl border-l ">Construction</span>
+        </div>
+        <div>
+        <form action="">
+        <input type="text" />
+        </form>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
