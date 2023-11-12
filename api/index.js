6a1b0db37d4e0a37a26d458c856bdee7cd56eef8
Jn-Sajol <julkar10121@gmail.com>
2023-11-12 07:45:34 +0600

    connect eoth mongo db

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
-import express from 'express'
-const app = express()
-const port = 8000
+import express from "express";
+const app = express();
+const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO).then(()=>{
+try {
+    console.log('db connet')
+} catch (error) {
+    console.log(error)
+}
 })
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/hello", (req, res) => {
+  res.send("hello from helllo site");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
