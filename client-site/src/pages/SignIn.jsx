09e2599f1412cafe8ebaf8d094c2416a6baa309e
Jn-Sajol <julkar10121@gmail.com>
2023-11-15 06:34:34 +0600

    complete  sign in page ui part

--- client-site/src/pages/SignIn.jsx
+++ client-site/src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-
+import {Link} from 'react-router-dom'
 const SignIn = () => {
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -9,6 +9,12 @@ const SignIn = () => {
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
         <button  className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
       </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
