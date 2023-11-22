bc273a21fd06538fb53b3aac97d07108d7fe4b1a
Jn-Sajol <julkar10121@gmail.com>
2023-11-22 08:56:46 +0600

    uder delete functionalitydone

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateUser } from "../controller/user.controller.js";
+import { deleteUser, updateUser } from "../controller/user.controller.js";
 import { verifyToken } from "../utils/varifyUser.js";
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/hello", (req, res) => {
 });
 
 router.patch('/update/:id',verifyToken, updateUser)
+router.delete('/delete/:id',verifyToken, deleteUser)
 
 export default router;
