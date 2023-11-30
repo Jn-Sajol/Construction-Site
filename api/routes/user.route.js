2e7c5fddc66d0893ad4b92b2d4dd4cf557812834
Jn-Sajol <julkar10121@gmail.com>
2023-11-30 22:13:41 +0600

    create api user listing api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,9 +3,9 @@ import { deleteUser, getUserListings, updateUser } from "../controller/user.cont
 import { verifyToken } from "../utils/varifyUser.js";
 const router = express.Router();
 
-router.get("/hello", (req, res) => {
-  res.send({hello:"hi"});
-});
+// router.get("/hello", (req, res) => {
+//   res.send({hello:"hi"});
+// });
 
 router.patch('/update/:id',verifyToken, updateUser)
 router.delete('/delete/:id',verifyToken, deleteUser)
