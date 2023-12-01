d34b1e2f3ff045c0f347dcc41ae7657081edf3c0
Jn-Sajol <julkar10121@gmail.com>
2023-12-01 22:17:29 +0600

    show user listing

--- client-site/src/pages/Profile.jsx
+++ client-site/src/pages/Profile.jsx
@@ -220,6 +220,43 @@ export default function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>
+      Show Listings
+    </button>
+    <p className='text-red-700 mt-5'>
+      {showListingsError ? 'Error showing listings' : ''}
+    </p>
+
+    {userListings &&
+      userListings.length > 0 &&
+      <div className="flex flex-col gap-4">
+        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+        {userListings.map((listing) => (
+          <div
+            key={listing._id}
+            className='border rounded-lg p-3 flex justify-between items-center gap-4'
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt='listing cover'
+                className='h-16 w-16 object-contain'
+              />
+            </Link>
+            <Link
+              className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+              to={`/listing/${listing._id}`}
+            >
+              <p>{listing.name}</p>
+            </Link>
+
+            <div className='flex flex-col item-center'>
+              <button className='text-red-700 uppercase'>Delete</button>
+              <button className='text-green-700 uppercase'>Edit</button>
+            </div>
+          </div>
+        ))}
+      </div>}
     </div>
   );
 }
