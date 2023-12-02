f320441dbb0e000a57e23dee6735399a980cd633
Jn-Sajol <julkar10121@gmail.com>
2023-12-02 22:38:59 +0600

    delete user listing

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { listing } from "../controller/listing.controller.js";
+import { deleteListing, listing } from "../controller/listing.controller.js";
 import { verifyToken } from "../utils/varifyUser.js";
 const router = express.Router();
 
 router.post('/create',verifyToken, listing)
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
--- client-site/src/pages/Profile.jsx
+++ client-site/src/pages/Profile.jsx
@@ -138,6 +138,24 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -220,43 +238,50 @@ export default function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
-      <button onClick={handleShowListings} className='text-green-700 w-full'>
-      Show Listings
-    </button>
-    <p className='text-red-700 mt-5'>
-      {showListingsError ? 'Error showing listings' : ''}
-    </p>
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsError ? "Error showing listings" : ""}
+      </p>
 
-    {userListings &&
-      userListings.length > 0 &&
-      <div className="flex flex-col gap-4">
-        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
-        {userListings.map((listing) => (
-          <div
-            key={listing._id}
-            className='border rounded-lg p-3 flex justify-between items-center gap-4'
-          >
-            <Link to={`/listing/${listing._id}`}>
-              <img
-                src={listing.imageUrls[0]}
-                alt='listing cover'
-                className='h-16 w-16 object-contain'
-              />
-            </Link>
-            <Link
-              className='text-slate-700 font-semibold  hover:underline truncate flex-1'
-              to={`/listing/${listing._id}`}
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
             >
-              <p>{listing.name}</p>
-            </Link>
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
 
-            <div className='flex flex-col item-center'>
-              <button className='text-red-700 uppercase'>Delete</button>
-              <button className='text-green-700 uppercase'>Edit</button>
+              <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>}
+          ))}
+        </div>
+      )}
     </div>
   );
 }
