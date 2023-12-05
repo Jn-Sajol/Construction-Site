1b132ccee06758bea15a659ab998e16944c8039c
Jn-Sajol <julkar10121@gmail.com>
2023-12-05 11:31:56 +0600

    listing update

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { deleteListing, listing } from "../controller/listing.controller.js";
+import { deleteListing, listing, updateListing } from "../controller/listing.controller.js";
 import { verifyToken } from "../utils/varifyUser.js";
 const router = express.Router();
 
 router.post('/create',verifyToken, listing)
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
